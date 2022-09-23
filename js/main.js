e3810080fbd3eb15c36840ae71e0613b45a7b09f
rudebeard <ademkoroglu542@gmail.com>
2022-09-22 21:26:21 -0400

    js part is coming

---
+++ js/main.js
@@ -0,0 +1,9 @@
+document.addEventListener('DOMContentLoaded',()=>{
+  loadFaq();
+})
+
+function loadFaq(){
+  const div = document.createElement('div');
+  div.classList.add('container');
+  div.innerHTML = ``
+}
